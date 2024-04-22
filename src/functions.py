7bfe5ae8a1ad9f0c4199c70fab4e41390eb51dcc
Maksim Netesov <MrEasters@mail.ru>
2024-04-22 20:17:45 +0300

    Initial project

---
+++ src/functions.py
@@ -0,0 +1,26 @@
+import json
+from datetime import datetime
+
+
+def mask_card_number(card_number):
+    """
+    Ваш код для маскирования номера карты
+    """
+
+
+def mask_account_number(account_number):
+    """
+    Ваш код для маскирования номера счёта
+    """
+
+
+def format_transaction(transaction):
+    """
+    Ваш код для форматирования одной транзакции
+    """
+
+
+def get_last_transactions():
+    """
+    Ваш код для извлечения и форматирования 5 последних операций
+    """
